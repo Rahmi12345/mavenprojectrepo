a1d288b4c911f0ec3bf80115970f980f127c1dcb
Asiel Safi <rahmisafi@Rahmis-iMac.lan>
2022-05-13 08:48:54 -0700

    this is maven repo

---
+++ src/main/java/us/tekschool/sharks/maven/us/tekschool/sharks/maven/App.java
@@ -0,0 +1,13 @@
+package us.tekschool.sharks.maven.us.tekschool.sharks.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
